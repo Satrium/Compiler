5783a2a1e183d432d31abe6f6f430d16ce48d1c9
Anton Pohli <anton.pohli@gmail.com>
2020-01-30 14:16:56 +0100

    Updated parser test

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -7,15 +7,15 @@ public class ParserTest {
 
     @Test()
     public void testForSucess() {
-        Parser parser = new Parser("(asdfg)");
-        parser.Start();
+        Parser parser = new Parser("(asdfg)#");
+        parser.start();
     }
 
     @Test()
     public void testForFail(){
-        Parser parser = new Parser("(asdfg.........)");
+        Parser parser = new Parser("(asdfg.........)#");
         Assertions.assertThrows(RuntimeException.class, () -> {
-            parser.Start();
+            parser.start();
         }); 
     }
 
