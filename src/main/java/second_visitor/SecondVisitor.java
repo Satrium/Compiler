d1771f050629fd2790de8dbfe5da01314a85cc3d
zalhfira <47207394+zalhfira@users.noreply.github.com>
2020-02-06 14:25:10 +0100

    visitor 2

--- src/main/java/second_visitor/SecondVisitor.java
+++ src/main/java/second_visitor/SecondVisitor.java
@@ -5,11 +5,19 @@ import syntaxtree.OperandNode;
 import syntaxtree.UnaryOpNode;
 import syntaxtree.Visitor;
 
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class SecondVisitor implements Visitor {
+    private SortedMap<Integer, FollowposTableEntry> followposTableEntries;
+
+    public SecondVisitor() {
+        followposTableEntries = new TreeMap<>();
+    }
 
     @Override
     public void visit(OperandNode node) {
-        
+
     }
 
     @Override
